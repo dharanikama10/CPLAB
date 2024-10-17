2b96c27f8438962f5806f3e1c7d940f1255f6e56
dharanikama10 <dharanikama109@gmail.com>
2024-10-17 16:38:01 +0530

    Add files via upload

---
+++ sqrt.c
@@ -0,0 +1,8 @@
+#include<math.h>
+int main(){
+    double num;
+    printf("enter nuumber");
+    scanf("%lf",&num);
+    double sqrt_num=sqrt(num);
+    printf("sqrt of %.2f is %.2f\n",num,sqrt);
+    }
